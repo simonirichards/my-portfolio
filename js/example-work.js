0706ef623af96d6ea7a0542173a9e163700da154
Simon Richards <simon.richards@digio.com.au>
2020-01-07 20:30:51 +1100

    Introduction of REACT work examples

---
+++ js/example-work.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class ExampleWork extends React.Component {
+    render() {
+        return (
+            
+            <section className="section section--alignCentered section--description">
+                
+                { this.props.work.map( (example, idx) => {
+                    return (
+                        <ExampleWorkBubble example={example} key={idx} />
+                        )
+                    })
+                }
+
+            </section>
+
+        )
+    }
+}
+
+class ExampleWorkBubble extends React.Component {
+    render() {
+        return (
+            <div className="section__exampleWrapper">
+                <div className="section__example">
+                    <img alt={ this.props.example.image.desc }
+                        className="section__exampleImage"
+                        src={ this.props.example.image.src }/>
+                    <dl className="color--cloud">
+                        <dt className="section__exampleTitle section__text--centered">
+                            { this.props.example.title }
+                        </dt>
+                        <dd></dd>
+                    </dl>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ExampleWork
